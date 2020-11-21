77e650a48366e1bae182479f167da037360c39b2
cassthallen <casserole181@yahoo.com>
2020-11-20 16:04:58 -0800

    Update websiteJava.js

--- websiteJava.js
+++ websiteJava.js
@@ -17,15 +17,18 @@ function draw()
 {
     background(0);
     fill(0);
+    
+    var hoveredElements = document.querySelectorAll(':hover'); // all the elements the mouse is currently hovering over
+    var currentElementMouseIsOver = document.querySelectorAll(':hover')[document.querySelectorAll(':hover').length-1]; // this is the last object in that hoveredElements array
 
-    if (mouseIsPressed) 
-        {
-            image(img1, 0, 0, 400, 400);
-            text("hewo c:", 170, 50, 100, 800);
-            textSize(25);
-        } 
+    if (mouseIsPressed && currentElementMouseIsOver.id == "defaultCanvas0") // <-- check if the mouse is hovering over the canvas
+    {
+        image(img1, 0, 0, 400, 400);
+        text("hewo c:", 170, 50, 100, 800);
+        textSize(25);
+    } 
     else 
-        {
-            image(img, 10, 10, 400, 400);
-        }
+    {
+        image(img, 10, 10, 400, 400);
+    }
 }
